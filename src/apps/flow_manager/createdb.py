2645ac98fd2698bcfbf4a23cdb6c9583e6b31a50
hariharaselvam <hariharaselvam8@gmail.com>
2016-10-03 18:17:33 +0530

    create test user from python updated

--- src/apps/flow_manager/createdb.py
+++ src/apps/flow_manager/createdb.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 dbhost = "localhost"
 dbport = "5984"
@@ -10,14 +11,13 @@ username = "testflowmgr@faucetsdn.org"
 password = "testflowmgr"
 role = "1"
 data = {"username": username, "password": password, "role": role}
-# createdb = requests.put("http://"+dbuser+":"+dbpass+"@"+dbhost+":"+dbport+"/"+userdatabase+"/")
-# print createdb
-view_data = "{_id: '_design/users',views: {'users': {'map': 'function(doc){ emit(doc._id, doc)}'}}}"
-createvw = requests.put(
-    url="http://" + dbuser + ":" + dbpass + "@" + dbhost + ":" + dbport + "/" + userdatabase + "/_design/users/",
+createdb = requests.put("http://"+dbuser+":"+dbpass+"@"+dbhost+":"+dbport+"/"+userdatabase+"/")
+print createdb
+view_data = {'_id': '_design/users','views': {'users': {'map': 'function(doc){ emit(doc._id, doc)}'}}}
+view_data = json.dumps(view_data)
+createvw = requests.put(url="http://" + dbuser + ":" + dbpass + "@" + dbhost + ":" + dbport + "/" + userdatabase + "/_design/users/",
     data=view_data)
-requests.put(url="http://" + dbuser + ":" + dbpass + "@" + dbhost + ":" + dbport + "/" + userdatabase + "/_design/users/",
-             data={'_id': '_design/users','views': {'users': {'map': 'function(doc){ emit(doc._id, doc)}'}}})
-print createvw.status_code, createvw.content
-# createuser =requests.post(url="http://"+dbhost+":"+dbport+"/"+userdatabase+"/", json={"username": username, "password": password, "role": role})
-# print createuser
+
+print createvw
+createuser =requests.post(url="http://"+dbhost+":"+dbport+"/"+userdatabase+"/", json={"username": username, "password": password, "role": role})
+print createuser
